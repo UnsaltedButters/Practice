427d26fb7be02c48e16f914dffcc633ce393304c
Wojciech Mosieński <w.mosienski@gmail.com>
2020-04-27 00:56:05 +0200

    asd

--- src/Mine.java
+++ src/Mine.java
@@ -2,13 +2,11 @@ public class Mine {
     public static void main(String[] args) {
         Double money;
         money = 200.0;
-        if (money >= 100) {
-            System.out.println("looks like you have a nice sum of money");
-        } else if (money >= 50) {
-            System.out.println("you might want to start to save up your money");
-
-        } else {
-            System.out.println("Get a job and make a living you donkey");
+        while (money >= 100) {
+            System.out.println("dawaj dyche");
+            money = money - 10;
         }
+
+        System.out.println("dalszy ciąg programu");
     }
 }
