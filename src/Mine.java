30f22ea8729a23d374d9caa959a1bcc082966ab3
UnsaltedButters <dawid.mosienski@gmail.com>
2020-04-26 17:48:26 -0500

    Hello

--- src/Mine.java
+++ src/Mine.java
@@ -1,8 +1,14 @@
 public class Mine {
     public static void main(String[] args) {
-        int variable;
-        // 12*14
-        variable = 14+2;
-        System.out.println(variable);
+        Double money;
+        money = 200.0;
+        if (money >= 100) {
+            System.out.println("looks like you have a nice sum of money");
+        } else if (money >= 50) {
+            System.out.println("you might want to start to save up your money");
+
+        } else {
+            System.out.println("Get a job and make a living you donkey");
+        }
     }
 }
