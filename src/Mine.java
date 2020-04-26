dc68e35d07dd322b2415b9e1009c6f38d37e95ac
UnsaltedButters <dawid.mosienski@gmail.com>
2020-04-26 18:31:35 -0500

    Hello

--- src/Mine.java
+++ src/Mine.java
@@ -1,7 +1,7 @@
 public class Mine {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i = i + 2) {
-            System.out.println(i);
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println((int) Math.pow(i,2));
         }
 
         System.out.println("dalszy ciąg programu");
